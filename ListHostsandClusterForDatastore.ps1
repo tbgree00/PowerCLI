fe253d2aab6f32f47b2101036632be77d2dc78a3
tbgree00 <thomgreene@icloud.com>
2016-09-16 17:04:56 -0400

    Add files via upload
    Detects vCenter connection status, prompts for a VMFS datastore to investigate, and writes Identifier information and outputs hosts attached and the cluster that host resides in.

---
+++ ListHostsandClusterForDatastore.ps1
@@ -0,0 +1,19 @@
+﻿Add-PSSnapin VMware.VimAutomation.Core
+
+#Check status of vCenter server and connect if disconnected
+if($global:DefaultVIServer.IsConnected){
+    Write-Host "Connected to vCenter"
+    }else{
+    Connect-VIServer (Read-Host "Enter vCenter Server")}
+
+
+#prompt for Datastore to investigate
+$ds = Get-Datastore (Read-Host "Enter Datastore")
+
+#Get Disk Identifier for a given Datastore Name#
+$ds.ExtensionData.Info.Vmfs.Extent.DiskName
+
+#Get Hosts accessing Datastore and output to a CSV File#
+$ds.ExtensionData.Host.Key | Get-VIObjectByVIView | Select Name, @{N=”Cluster”;E={Get-Cluster -VMHost $_}}| Export-CSV $ds-Hosts.csv
+$Filepath = Get-Location
+Write-Host File $ds-Hosts.csv written to $Filepath.Path
